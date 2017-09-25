e5c1c1ccd6556844c0d1f6cd455dd83ff91eaf35
DanieleDiLucido <danieledilucido@gmail.com>
2017-09-25 10:34:24 +0200

    Fix service getRndDate

--- index.js
+++ index.js
@@ -1,50 +1,30 @@
 var express = require('express');
 var moment = require('moment');
-var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({ port: 9000 });
 var app = express();
+const getDate = (()=> {
+  let myDate =  moment().add(Math.floor(Math.random() * 60) + 30,'second');
+  return ()=> {
+      if(myDate.isSameOrBefore(moment())){
+          myDate =  moment().add(Math.floor(Math.random() * 60) + 30,'second');
+          console.log("We have a new Date dude!");
+      }
+      return myDate;
+  }
+})();
 
-var server = app.listen(8000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("REST API listening on http://%s:%s", host, port)
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/getRndDate', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(JSON.stringify({date:getDate().format()}));
 });
 
-app.get('/', function (req, res) {
-  console.log('REST Server received GET')
-  wss.clients.forEach(function each(client) {
-      client.send("Broadcast to client: REST Server received GET");
-    });
-  res.sendStatus(200);
-})
-
-
-
-// const getDate = (()=> {
-//   let myDate =  moment().add(Math.floor(Math.random() * 60) + 30,'second');
-//   return ()=> {
-//       if(myDate.isSameOrBefore(moment())){
-//           myDate =  moment().add(Math.floor(Math.random() * 60) + 30,'second');
-//           console.log("We have a new Date dude!");
-//       }
-//       return myDate;
-//   }
-// })();
-
-
-// app.set('port', (process.env.PORT || 5000));
-
-// app.use(express.static(__dirname + '/public'));
-
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-// app.get('/getRndDate', function (req, res) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.send(JSON.stringify({date:getDate().format()}));
-// });
-
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
